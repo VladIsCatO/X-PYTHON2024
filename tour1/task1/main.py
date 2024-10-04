a80d20958d7cd3b8961310ddd5d8fc7d7d23a7b9
DregoScat <vladyslav.bankovskyi@gmail.com>
2024-10-04 18:46:07 +0200

    task2

--- tour1/task1/main.py
+++ tour1/task1/main.py
@@ -1,35 +1,93 @@
 #Imports section
 import turtle
-from turtle import *
+from turtle import Turtle, screensize
 import random
+from time import sleep
 
 #---------------------------------------------
 #INIT
 wn = turtle.Screen() #Starting screen
+wn.setup(1080, 560)
 
 # This turns off screen updates 
 wn.tracer(0) 
 
-
-
+drawer = Turtle() #declaring our drawer of the level
+writer = Turtle() #writer will write text
+writer.penup()
+writer.hideturtle()
+writer.goto(screensize()[0]-screensize()[0]/4, screensize()[1]-screensize()[1]/0.6)
 #---------------------------------------------
 #Functions helpers
 
 def replace(string, to, index):
     return string[:index] + to + string[index + 1:] #I took this part from stack overflow
 
+def is_collided_with(player_coords:tuple, objects_coords:dict): #Taken from stack overflow and changed
+    for i in range(len(objects_coords)):
+        print(i)
+        if abs(player_coords[0] - list(objects_coords.keys())[i][0]) < 10 and abs(player_coords[1] - list(objects_coords.keys())[i][1]) < 10:
+            print(f'collision with {list(objects_coords.values())[i]}')
+        else:
+            print('no')
+
 #---------------------------------------------
 #Classes
 
 class Player():
     def __init__(self) -> None:
+        #declaring player's variables
+        self.turtle_player = Turtle()
         self.inventory = []
+
+        #applying all the properties I need to our turtle player
+        self.turtle_player.showturtle()
+        self.turtle_player.penup()
+        self.turtle_player.speed(0)
+    
+    def TurtlePlayer(self) -> Turtle:
+        return self.turtle_player
         
-    def AddItemToInventory(self, item):
+    def AddItemToInventory(self, item) -> None:
         self.inventory.append(item)
     
-    def GetInventory(self):
+    def GetInventory(self) -> list:
         return self.inventory
+    
+    def CheckForWall(self, action: str) -> None:
+        pass
+
+    def CheckObjectCollision(self, objects_positions:dict) -> None:
+        position_ = self.turtle_player.position()
+        is_collided_with(position_,objects_positions)
+    
+    def left(self) -> None:
+        # print('left')
+        self.turtle_player.left(5)
+        self.CheckForWall('left')
+        self.CheckObjectCollision(positions)
+    
+    def right(self) -> None:
+        # print('right')
+        self.turtle_player.right(5)
+        self.CheckForWall('right')
+        self.CheckObjectCollision(positions)
+    
+    def forward(self) -> None:
+        # print('forward')
+        self.turtle_player.forward(3)
+        self.CheckForWall('forward')
+        self.CheckObjectCollision(positions)
+
+    def back(self) -> None:
+        # print('back')
+        self.turtle_player.back(3)
+        self.CheckForWall('back')
+        self.CheckObjectCollision(positions)
+    
+    
+
+    
 
 
 #---------------------------------------------
@@ -86,71 +144,85 @@ def placeObjects():
                 
 
 
-
+positions = {}
 #drawing level
 def drawLevel():
+    global positions
     #placing objects
     placeObjects()
     #preparations to draw level
-    penup()
-    left(90)
-    forward(len(level)/2*UNIT)
-    left(90)
-    forward(len(level[0])/2*UNIT)
-    right(180)
+    drawer.penup()
+    drawer.left(90)
+    drawer.forward(len(level)/2*UNIT)
+    drawer.left(90)
+    drawer.forward(len(level[0])/2*UNIT)
+    drawer.right(180)
+
+    positions = {} #a dict for positions of objects
 
     for line in enumerate(level):
         for symbol in enumerate(line[1]):
             if level[line[0]-1][symbol[0]] == 'X' and symbol[1] == 'X' and line[0] != 0:
-                pendown()
-                left(90)
-                forward(UNIT)
-                right(180)
-                forward(UNIT)
-                left(90)
-                penup()
+                drawer.pendown()
+                drawer.left(90)
+                drawer.forward(UNIT)
+                drawer.right(180)
+                drawer.forward(UNIT)
+                drawer.left(90)
+                drawer.penup()
                 try:
                     if level[line[0]][symbol[0]+1] == 'X':
-                        pendown()
-                        forward(UNIT)
-                        penup()
+                        drawer.pendown()
+                        drawer.forward(UNIT)
+                        drawer.penup()
                     else:
-                        forward(UNIT)
+                        drawer.forward(UNIT)
                 except:
-                    forward(UNIT)
+                    drawer.forward(UNIT)
             elif symbol[1] == 'X':
-                pendown()
-                forward(UNIT)
-                penup()
+                drawer.pendown()
+                drawer.forward(UNIT)
+                drawer.penup()
             elif symbol[1] == ' ':
-                forward(UNIT)
+                drawer.forward(UNIT)
             else:
-                forward(UNIT)
+                drawer.write(symbol[1])
+                drawer.forward(UNIT/2)
+                positions[drawer.pos()] = symbol[1]
+                drawer.forward(UNIT/2)
         if level[line[0]][0] == 'X':
             try:
-                right(90)
+                drawer.right(90)
                 # pendown()
-                forward(UNIT)
-                right(90)
-                penup()
-                forward(len(line[1])*UNIT)
-                left(180)
+                drawer.forward(UNIT)
+                drawer.right(90)
+                drawer.penup()
+                drawer.forward(len(line[1])*UNIT)
+                drawer.left(180)
             except:
                 pass
 
+    print(positions)
+
 
 drawLevel()
+drawer.hideturtle()
 
 # Update the screen to see the changes   
 wn.update() 
 
+#declaring player
+wn.tracer(1)
+
+player = Player()
 
 
-
-wn.tracer(1) 
-
-
-
+#listening to player controls
+wn.onkeypress(player.forward, 'w')
+wn.onkeypress(player.back, 's')
+wn.onkeypress(player.left, 'a')
+wn.onkeypress(player.right, 'd')
+wn.listen()
 
 # Keep the window open
 wn.mainloop() 
---
+++ tour1/task2/main.py
@@ -0,0 +1,54 @@
+"""               ___________________
+                -| Python Converter |-
+                 ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"""
+from ast import literal_eval
+import sys
+def retry():
+    """Code to ask if user wants to retry converting"""
+    again = input('retry (Y/n): ').strip().upper()
+    if again == 'Y':
+        return main()
+    sys.exit()
+
+def main():
+    """Main function"""
+    #Describing App
+    print("""
+                ___________________
+                -| Python Converter |-
+                ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+    !For now we only support foot, yards and miles!
+    1. Enter number, supported unit FROM what you wanna convert and supported unit TO what you wanna convert,
+    example:
+    15 ft yd
+    --> Result: 5 yards
+    2. Get your result.
+
+        HELPER:
+    foot = ft;
+    yard = yd;
+    miles = mi.""")
+    unit_signs = {'ft_yd':'/3',
+                'ft_mi':'/5208',
+                'mi_ft':'*5280',
+                'mi_yd':'*1760',
+                'yd_ft':'*3',
+                'yd_mi':'*1760'}
+    user = input('>').strip().split()
+    if len(user) == 3:
+        try:
+            print(0)
+            number = int(user[0])
+            from_unit = user[1]
+            to_unit = user[2]
+            print(1)
+        except ValueError:
+            print('Error: Not right value. Watch example again and retry.')
+            retry()
+        print(from_unit)
+        sign = unit_signs[f'{from_unit}_{to_unit}']
+        return literal_eval(str(number)+sign)
+    print('Error: Less numbers than expected. Watch example again and retry.')
+    return retry()
+
+print(main())
